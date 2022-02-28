ff9f17cd459cc8d5fa9b8b8e56d7e22eeb57efe4
Amal Djibo <amaldjibo45@gmail.com>
2022-02-28 16:31:15 -0500

    I made a mad  lib

---
+++ Lesson 8 - Mad Libs Game/main.py
@@ -0,0 +1,9 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+action_1 = input("Enter Action 1: ")
+action_2 = input("Enter Action 2: ")
+
+print("Roses are " + color)
+print(plural_noun  + " are blue")
+print("You " +  action_1  + " Me")
+print("Now I " + action_2 + " You")
