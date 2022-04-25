f61695375715edc6c86a6888e8416d8cd7b608c6
Amal Djibo <amaldjibo45@gmail.com>
2022-04-25 19:55:07 +0000

    Learned about variables

---
+++ Lesson 2/main.py
@@ -0,0 +1,17 @@
+# Making a robot barista
+
+print("Hello welcome to Amal's Coffee!!!!!")
+
+
+name = input("What's your name? \n\n\n") 
+
+print("Hello " + name + ", thanks for coming in today. A\n\n\n")
+
+
+menu = "Black Cofee, Espresso, Coke"
+
+print(name + ", what are you ordering today? Here's what we are offering: \n" + menu)
+
+order = input()
+
+print(name + ", we'll have that " + order + " ready for you soon.")
