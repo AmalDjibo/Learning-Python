e30a1688e71ef35d1918bc2435de682ab5056872
unknown <amaldjibo45@gmail.com>
2022-09-04 12:19:28 -0400

    starting over

---
+++ Lesson 1 - Variables/main.py
@@ -0,0 +1,6 @@
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = "Python for Noobs"
+print(students_count)
+print(course_name)
--- Lesson 1/main.py
+++
@@ -1,5 +0,0 @@
-#print("Hello World!!!")
-#print('I am Iron Man!')
-# I am learning #
-
-print("I am Iron Man \n" + "No, I am Tony Stark\n" * 100)
